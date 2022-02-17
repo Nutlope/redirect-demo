1eec75cad21d3076253d7f1058eda56dc2352a75
nutlope <he55@drexel.edu>
2022-02-16 20:37:51 -0500

    it worked??

--- next.config.js
+++ next.config.js
@@ -3,10 +3,6 @@ module.exports = {
   async rewrites() {
     return [
       {
-        source: "/blog",
-        destination: "https://blog-svix.vercel.app/blog",
-      },
-      {
         source: "/blog/:slug",
         destination: "https://blog-svix.vercel.app/blog/:slug",
       },
