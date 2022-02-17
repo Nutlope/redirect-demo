891256c1c5f0b197e0fcdece83270fed5f2994fe
nutlope <he55@drexel.edu>
2022-02-16 20:49:35 -0500

    trying with diff blog

--- next.config.js
+++ next.config.js
@@ -3,13 +3,13 @@ module.exports = {
   // next.js rewrite to redirect /blog to an external blog
   async rewrites() {
     return [
-      {
-        source: "/blog",
-        destination: "https://blog-svix.vercel.app/blog",
-      },
+      //   {
+      //     source: "/blog",
+      //     destination: "https://blog-svix.vercel.app/blog",
+      //   },
       {
         source: "/blog/:slug",
-        destination: "https://blog-svix.vercel.app/blog/:slug",
+        destination: "https://www.cstrnt.dev/blog/:slug",
       },
     ];
   },
