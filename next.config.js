1a246340f37bb9017c6f3097bd01c87b71e07990
nutlope <he55@drexel.edu>
2022-02-16 21:06:18 -0500

    trying

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 module.exports = {
   async rewrites() {
     return [
-      //   {
-      //     source: "/blog",
-      //     destination: "https://www.cstrnt.dev",
-      //   },
+      {
+        source: "/blog",
+        destination: "https://www.cstrnt.dev",
+      },
       {
         source: "/blog/:slug",
         destination: "https://www.cstrnt.dev/blog/:slug*",
