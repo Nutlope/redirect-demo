ac6a3e078b42cae6110cd4b94081c024eb45df8b
nutlope <he55@drexel.edu>
2022-02-17 10:36:26 -0500

    testing new. WORKING!

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 module.exports = {
   async redirects() {
     return [
-      {
-        source: "/blog",
-        destination: "https://blog-svix.vercel.app/blog/",
-        permanent: true,
-      },
+      //   {
+      //     source: "/blog",
+      //     destination: "https://blog-svix.vercel.app/blog/",
+      //     permanent: true,
+      //   },
       {
         source: "/blog/:slug",
         destination: "https://blog-svix.vercel.app/blog/:slug",
