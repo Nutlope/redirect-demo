9e53d89f6a1c64595f8714d7ece3a7a92731890a
nutlope <he55@drexel.edu>
2022-02-23 10:20:21 -0500

    its working!

--- next.config.js
+++ next.config.js
@@ -1,15 +1,16 @@
 const BLOG_URL = "https://starter-blog-example.vercel.app";
 
 module.exports = {
+  trailingSlash: true,
   async rewrites() {
     return [
       {
-        source: "/blog",
-        destination: `${BLOG_URL}/blog`,
+        source: "/blog/",
+        destination: `${BLOG_URL}/blog/`,
       },
       {
-        source: "/blog/:slug",
-        destination: `${BLOG_URL}/blog/:slug`,
+        source: "/blog/:path*",
+        destination: `${BLOG_URL}/blog/:path*`, // This should copy the slash from :path*
       },
     ];
   },
