7a105c5078727d45fe370d88d300c0dbaf320bf8
nutlope <he55@drexel.edu>
2022-02-23 11:02:40 -0500

    working demo

--- next.config.js
+++ next.config.js
@@ -1,7 +1,47 @@
 const BLOG_URL = "https://starter-blog-example.vercel.app";
 
-module.exports = {
-  trailingSlash: true,
+const rehypePrism = require("@mapbox/rehype-prism");
+const withImages = require("next-images");
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+const withMDX = require("@next/mdx")({
+  extension: /\.mdx?$/,
+  options: {
+    rehypePlugins: [rehypePrism],
+  },
+});
+module.exports = withImages({
+  ...withBundleAnalyzer({
+    images: {
+      disableStaticImages: true,
+    },
+    reactStrictMode: true,
+    trailingSlash: true,
+    ...withMDX({
+      pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
+    }),
+  }),
+  async redirects() {
+    return [
+      {
+        source: "/slack",
+        destination:
+          "https://join.slack.com/t/svixcommunity/shared_invite/zt-qq8tk1jn-NM4uhijSxt6IGa4qwkBeYQ;",
+        permanent: false,
+      },
+      {
+        source: "/signup",
+        destination: "https://dashboard.svix.com/accounts/signup",
+        permanent: false,
+      },
+      {
+        source: "/docs/:path*",
+        destination: "https://docs.svix.com/:path*",
+        permanent: false,
+      },
+    ];
+  },
   async rewrites() {
     return [
       {
@@ -10,8 +50,8 @@ module.exports = {
       },
       {
         source: "/blog/:path*",
-        destination: `${BLOG_URL}/blog/:path*`, // This should copy the slash from :path*
+        destination: `${BLOG_URL}/blog/:path*`,
       },
     ];
   },
-};
+});
